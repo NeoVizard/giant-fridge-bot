fb0b3a79e8b91b3bd88af7741fac2fc5cd6593f1
Nirmal Agrawal <nprabhat97@gmail.com>
2021-06-16 17:14:13 +0530

    loop

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 import requests
 import os
-# import sys
+import time
 import json
 from dotenv import load_dotenv
 load_dotenv()
 
 headers = {"Authorization": "Bearer " + os.getenv("BEARER")}
-
 # %23 is '#'
 url = "https://api.twitter.com/2/tweets/search/recent?query=%23art has:images -is:reply -is:quote -is:retweet&expansions=attachments.media_keys&tweet.fields=referenced_tweets,public_metrics&media.fields=media_key,type,url&max_results=10"
 
-r = requests.get(url, headers=headers)
-res = r.json()
+while True:
+    r = requests.get(url, headers=headers)
+    res = r.json()
 
-# sys.stdout.reconfigure(encoding='utf-8')
-# # print(r.status_code)
-# print(json.dumps(r.json()))
+    tweets = res["data"]
+    media = res["includes"]["media"]
+    meta = res["meta"]
 
-# print(res)
+    stweet = sorted(tweets, key=lambda t: t["public_metrics"]["like_count"], reverse=True)
 
-tweets = res["data"]
-media = res["includes"]["media"]
-meta = res["meta"]
+    media_keys = [twit["attachments"]["media_keys"][0] for twit in stweet[:5]]
 
-stweet = sorted(tweets, key=lambda t: t["public_metrics"]["like_count"], reverse=True)
+    imgs = list(filter(lambda x: x["media_key"] in media_keys, media))
 
-media_keys = [twit["attachments"]["media_keys"][0] for twit in stweet[:5]]
+    data = {"images": imgs}
 
-imgs = list(filter(lambda x: x["media_key"] in media_keys, media))
+    res = requests.put('https://giant-fridge-92b65-default-rtdb.firebaseio.com/data.json', json.dumps(data))
 
-data = {"images": imgs}
-
-res = requests.put('https://giant-fridge-92b65-default-rtdb.firebaseio.com/data.json', json.dumps(data))
-
-print(res.status_code)
+    print(res.status_code)
+    print('Sleeping for 15 seconds...')
+    time.sleep(15)
