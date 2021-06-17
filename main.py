d2dd19be6c1ce05624aa6eb97701bf2b7799c168
Nirmal Agrawal <nprabhat97@gmail.com>
2021-06-17 20:33:30 +0530

    updated tweet data structure, start and end time

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ import time
 import json
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+
 # from keep_alive import keep_alive
 
 load_dotenv()
 
 retry_count = 2
 headers = {"Authorization": "Bearer " + os.getenv("BEARER")}
-to_date = datetime.utcnow() - timedelta(days=1)
+from_date = datetime.utcnow() - timedelta(days=3)
+from_date = from_date.replace(microsecond=0)
+start_time = f"&start_time={from_date.isoformat()}Z"
+to_date = datetime.utcnow() - timedelta(days=2)
 to_date = to_date.replace(microsecond=0)
+end_time = f"&end_time={to_date.isoformat()}Z"
+
 
 # %23 is '#'
-url = f"https://api.twitter.com/2/tweets/search/recent?query=%23art %23sega has:images -is:reply -is:quote -is:retweet&end_time={to_date.isoformat()}Z&expansions=attachments.media_keys&tweet.fields=referenced_tweets,public_metrics&media.fields=media_key,type,url&max_results=100"
+url = f"https://api.twitter.com/2/tweets/search/recent?query=%23art has:images -%23nsfw -%23r34 -is:reply -is:quote -is:retweet{start_time}{end_time}&expansions=attachments.media_keys&tweet.fields=public_metrics&media.fields=media_key,type,url&max_results=100"
+
 tweet_count = 100
 
 # keep_alive()
@@ -25,19 +32,19 @@ while True:
 
         meta = res["meta"]
 
-        if (meta["result_count"] > 0):
+        if meta["result_count"] > 0:
             tweets = res["data"]
             media = res["includes"]["media"]
 
             while "next_token" in meta:
                 new_url = f"{url}&next_token={meta['next_token']}"
                 r = requests.get(new_url, headers=headers)
-                if(r.status_code == 200):
-                    tweet_count+=100
+                if r.status_code == 200:
                     print(f"Fetched {tweet_count} tweets...")
                     res = r.json()
                     meta = res["meta"]
                     if meta["result_count"] > 0:
+                        tweet_count += 100
                         tweets += res["data"]
                         media += res["includes"]["media"]
                 else:
@@ -45,22 +52,29 @@ while True:
                     meta = {}
 
             print(f"Successfully fetched {len(tweets)} tweets")
-            sorted_tweets = sorted(
+            top_tweets = sorted(
                 tweets, key=lambda t: t["public_metrics"]["like_count"], reverse=True
-            )
+            )[:7]
 
-            media_keys = [t["attachments"]["media_keys"][0] for t in sorted_tweets[:5]]
+            media_keys = [t["attachments"]["media_keys"][0] for t in top_tweets]
 
             imgs = list(filter(lambda x: x["media_key"] in media_keys, media))
 
-            data = {"images": imgs}
+            for tweet in top_tweets:
+                tweet["media"] = next(
+                    x
+                    for x in imgs
+                    if x["media_key"] == tweet["attachments"]["media_keys"][0]
+                )
+
+            data = {"top_tweets": top_tweets}
 
             r = requests.put(
                 "https://giant-fridge-92b65-default-rtdb.firebaseio.com/data.json",
                 json.dumps(data),
             )
 
-            if(r.status_code == 200):
+            if r.status_code == 200:
                 print("Successfully posted data to firebase")
             else:
                 print("Failed to post data to firebase")
